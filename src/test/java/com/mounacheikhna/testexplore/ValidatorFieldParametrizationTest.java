1267856f2ffd65d82067191ed0888569f53b3a2e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-02-01 17:50:36 +0000

    Use parameters validation.

--- src/test/java/com/mounacheikhna/testexplore/ValidatorFieldParametrizationTest.java
+++ src/test/java/com/mounacheikhna/testexplore/ValidatorFieldParametrizationTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
@@ -11,26 +12,23 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-
 @RunWith(Parameterized.class)
-public class ValidatorTest {
+public class ValidatorFieldParametrizationTest {
 
+    @Parameter
     public Extra input;
-    private boolean validationResult;
+
+    @Parameter(1)
+    public boolean validationResult;
+
     private Validator validator;
 
     @Parameters(name = "{index}: validate({0})={1}") // not sure about this notation ?
     public static List<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                { new Extra("integer", "1"), true}, {new Extra("boolean", "true"), true },
-                { new Extra("integer", "a1"), false}, {new Extra("boolean", "b1"), false }
+                {new Extra("integer", "1"), true}, {new Extra("boolean", "true"), true},
+                {new Extra("integer", "a1"), false}, {new Extra("boolean", "b1"), false}
         });
-    }
-
-    // this is not ideal ?
-    public ValidatorTest(Extra input, boolean validationResult) {
-        this.input = input;
-        this.validationResult = validationResult;
     }
 
     @Before
